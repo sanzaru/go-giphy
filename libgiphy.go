cb77f670e543da4eb6ceacfb4e055dd4833c9f9b
Martin <mbr@Martins-iMac.local>
2017-03-20 15:56:09 +0100

    Fixed formatting

--- libgiphy.go
+++ libgiphy.go
@@ -77,37 +77,37 @@ type giphyDataArray struct {
 // giphyDataArray is a struct holding a single API result entry
 type giphyDataSingle struct {
     Data struct {
-             Type, Id, Slug, Url, Bitly_gif_url, Bitly_url, Embed_url,
-             Username, Source, Rating, Caption, Content_url, Source_tld,
-             Source_post_url, Import_datetime, Trending_datetime string
+        Type, Id, Slug, Url, Bitly_gif_url, Bitly_url, Embed_url,
+        Username, Source, Rating, Caption, Content_url, Source_tld,
+        Source_post_url, Import_datetime, Trending_datetime string
 
-             Images struct {
-                 Fixed_height gipyImageDataExtended
-                 Fixed_height_still gipyImageData
-                 Fixed_height_downsampled gipyImageDataExtended
-                 Fixed_width gipyImageDataExtended
-                 Fixed_width_still gipyImageData
-                 Fixed_width_downsampled gipyImageDataExtended
-                 Fixed_height_small gipyImageDataExtended
-                 Fixed_height_small_still gipyImageData
-                 Fixed_width_small gipyImageDataExtended
-                 Fixed_width_small_still gipyImageData
-                 Downsized gipyImageDataSized
-                 Downsized_still gipyImageData
-                 Downsized_large gipyImageDataSized
-                 Original gipyImageDataExtendedFrames
-                 Original_still gipyImageData
-             }
+        Images struct {
+            Fixed_height gipyImageDataExtended
+            Fixed_height_still gipyImageData
+            Fixed_height_downsampled gipyImageDataExtended
+            Fixed_width gipyImageDataExtended
+            Fixed_width_still gipyImageData
+            Fixed_width_downsampled gipyImageDataExtended
+            Fixed_height_small gipyImageDataExtended
+            Fixed_height_small_still gipyImageData
+            Fixed_width_small gipyImageDataExtended
+            Fixed_width_small_still gipyImageData
+            Downsized gipyImageDataSized
+            Downsized_still gipyImageData
+            Downsized_large gipyImageDataSized
+            Original gipyImageDataExtendedFrames
+            Original_still gipyImageData
+        }
 
-             Meta struct {
-                 Status int
-                 Msg string
-             }
+        Meta struct {
+            Status int
+            Msg string
+        }
 
-             Pagination struct {
-                 Total_count, Count, Offset int
-             }
-         }
+        Pagination struct {
+            Total_count, Count, Offset int
+        }
+    }
 }
 
 
