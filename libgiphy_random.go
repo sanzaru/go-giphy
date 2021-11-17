164780f8cacc61008c0046ca75eace26a5b0a58d
Roman Serov <roman@serov.co>
2021-11-17 15:59:35 +0300

    Random endpoint fix

--- libgiphy_random.go
+++ libgiphy_random.go
@@ -10,45 +10,25 @@
 package libgiphy
 
 import (
-    "encoding/json"
-    "net/url"
+	"encoding/json"
+	"net/url"
 )
 
 //
 // Data type for API data
 //
-type giphyDataRandom struct {
-    Data struct {
-         Type, Id, Url, Image_original_url, Image_url,
-         Image_mp4_url, Image_frames, Image_width,
-         Image_height, Fixed_height_downsampled_url,
-         Fixed_height_downsampled_width, Fixed_height_downsampled_height,
-         Fixed_width_downsampled_url, Fixed_width_downsampled_width,
-         Fixed_width_downsampled_height, Fixed_height_small_url,
-         Fixed_height_small_still_url, Fixed_height_small_width,
-         Fixed_height_small_height, Fixed_width_small_url,
-         Fixed_width_small_still_url, Fixed_width_small_width,
-         Fixed_width_small_height string
-    }
 
-    Meta struct {
-        Status int
-        Msg string
-    }
-}
+func (g *Giphy) GetRandom(tag string) (*giphyDataSingle, error) {
+	body, err := g._fetch(g._buildUrl("random") + "&tag=" + url.QueryEscape(tag))
+	if err != nil {
+		return nil, err
+	}
 
+	var data giphyDataSingle
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, err
+	}
 
-func (g * Giphy) GetRandom(tag string) (*giphyDataRandom, error) {
-    body, err := g._fetch(g._buildUrl("random") + "&tag=" + url.QueryEscape(tag))
-    if err != nil {
-        return nil, err
-    }
-
-    var data giphyDataRandom
-    err = json.Unmarshal(body, &data)
-    if err != nil {
-        return nil, err
-    }
-
-    return &data, nil
+	return &data, nil
 }
